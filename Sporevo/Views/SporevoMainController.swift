04f205c3f670b2e6615e6d84cdab38b35010cedb
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-11-08 15:49:42 +0900

    ready initial viewcontroller

---
+++ Sporevo/Views/SporevoMainController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class SporevoMainController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
