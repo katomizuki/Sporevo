f5d3ef2f2272314286e9a3dff701107d936cc857
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-01-22 09:50:18 +0900

    setup searchFunction

--- Sporevo/Model/APIDB/Facility.swift
+++ Sporevo/Model/APIDB/Facility.swift
@@ -51,12 +51,13 @@ struct FetchFacility: FetchFacilityInputs {
     func fetchFacility(completion: @escaping (Result<Facilities, Error>) -> Void) {
         let header:HTTPHeaders = ["Authorization":"Token LIcCke0gTSNAloR7ptYq"]
         let queri = makeCityQueri() + makeFacilityQueri() + makeTagQueri() + makePriceQueri() + makeSportQueri()
-        //        \(queri)/size=10&page=1
-        let baseURL = "https://spo-revo.com/api/v1/facilities?size=2000&page=1"
+//                \(queri)/size=10&page=1
+        let baseURL = "https://spo-revo.com/api/v1/facilities?\(queri)size=10&page=1"
         AF.request(baseURL, method: .get, parameters: nil, encoding: URLEncoding.default, headers: header).responseJSON { response in
             guard let data = response.data else { return }
             do {
                 let decodeData = try JSONDecoder().decode(Facilities.self, from: data)
+                print(decodeData,"⚡️")
                 completion(.success(decodeData))
             } catch {
                 completion(.failure(error))
--- Sporevo/Views/Controller/SearchMapController.swift
+++ Sporevo/Views/Controller/SearchMapController.swift
@@ -28,7 +28,7 @@ class SearchMapController:UIViewController {
         let allDetail: [FacilityDetail] = realm.objects(FacilityDetailEntity.self).map({
             FacilityDetail(access: $0.access, address: $0.address, facility_type: $0.facilityType, group_use_regist: $0.groupUseRegist, group_use_regist_text: $0.personalUseRegistText, lat: $0.lat, lng: $0.lng, name: $0.name, booking_url: $0.bookingUrl, business_hours: $0.businessHours, holiday: $0.holiday, how_to_book: $0.howToBook, hp: $0.hp, informer: $0.informer, memo: $0.memo, phone_number: $0.phoneNumber, price_info: $0.priceInfo, personal_use_regist: $0.personalUseRegist, personal_use_regist_text: $0.personalUseRegistText, sub_name: $0.subName, booking_types: [], equipment_types: [], parking_types: [], price_ranges: [], sports_types: [], tags: [], updated_at:"", user_types: [])
         })
-        
+        mapView.removeAnnotations(mapView.annotations)
         allDetail.forEach {
             let coorinate = CLLocationCoordinate2DMake($0.lat ?? 0.0, $0.lng ?? 0.0)
             let pin = MKPointAnnotation()
