a9c35cfa578cfdb891503f3bf05ecd3585812184
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-01-28 21:31:08 +0900

    set up basic viewModel

--- Sporevo/Presentar/ViewModel/SporevoMainViewModel.swift
+++ Sporevo/Presentar/ViewModel/SporevoMainViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import RxSwift
+import ReSwift
+import RxRelay
+
+protocol SporevoMainViewModelInputs {
+    var willAppear: PublishRelay<Void> { get }
+    var willDisAppear: PublishRelay<Void> { get }
+}
+
+protocol SporevoMainViewModelOutputs {
+    
+}
+
+protocol SporevoMainViewModelType {
+    var inputs: SporevoMainViewModelInputs { get }
+    var outputs: SporevoMainViewModelOutputs { get }
+}
+
+final class SporevoMainViewModel:SporevoMainViewModelType, SporevoMainViewModelOutputs,SporevoMainViewModelInputs {
+    var willAppear = PublishRelay<Void>()
+    
+    var willDisAppear = PublishRelay<Void>()
+    
+    
+    var inputs: SporevoMainViewModelInputs { return self }
+    var outputs: SporevoMainViewModelOutputs { return self }
+    
+    private let disposeBag = DisposeBag()
+    private let store:Store<AppState>
+    
+    init(store: Store<AppState>) {
+        self.store = store
+        
+        willAppear.subscribe(onNext: { [unowned self] _ in
+            self.store.subscribe(self) { subcription in
+                subcription.select { state in state.mapState }
+            }
+        }).disposed(by: disposeBag)
+        
+        willDisAppear.subscribe(onNext: { [unowned self] _ in
+            self.store.unsubscribe(self)
+        }).disposed(by: disposeBag)
+
+
+        
+    }
+    
+    
+}
+extension SporevoMainViewModel: StoreSubscriber {
+    
+    typealias StoreSubscriberStateType = SporevoMapState
+    
+    func newState(state: SporevoMapState) {
+        
+    }
+}
